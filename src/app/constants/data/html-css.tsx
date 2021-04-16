98cbdeebf87f6ebc7802ecda93940d39a0148113
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-04-16 12:14:21 +0300

    feat: new article

--- src/app/constants/data/html-css.tsx
+++ src/app/constants/data/html-css.tsx
@@ -2,6 +2,33 @@ import React, { ReactElement } from 'react';
 import s from './styles.scss';
 
 export const HtmlCssTopics = {
+  responsiveDesign: {
+    title: 'Responsive design',
+    content: (): ReactElement => (
+      <div className={s.content}>
+        <ul>
+          <li>
+            <a href="https://kinsta.com/blog/responsive-web-design/" target="_blank" rel="noreferrer">
+              Responsive web design
+            </a>
+          </li>
+
+          <li>
+            Recorded lesson
+            <iframe
+              style={{ width: '100%', marginTop: '15px' }}
+              height="440"
+              src="https://www.youtube.com/embed/o2uOqyKjU4M"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </li>
+        </ul>
+      </div>
+    ),
+  },
   selectors: {
     title: 'Selectors',
     content: (): ReactElement => (
