ea87a2cf5ddf7808db4c8d6f0d557ec827572dae
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-04-17 23:35:34 +0300

    feat: new

--- src/app/constants/data/html-css.tsx
+++ src/app/constants/data/html-css.tsx
@@ -2,6 +2,33 @@ import React, { ReactElement } from 'react';
 import s from './styles.scss';
 
 export const HtmlCssTopics = {
+  preprocessors: {
+    title: 'Sass',
+    content: (): ReactElement => (
+      <div className={s.content}>
+        <ul>
+          <li>
+            <a href="https://sass-scss.ru/guide/" target="_blank" rel="noreferrer">
+              Sass Basics
+            </a>
+          </li>
+
+          <li>
+            Recorded lesson
+            <iframe
+              style={{ width: '100%', marginTop: '15px' }}
+              height="440"
+              src="https://youtu.be/UlEKQXylZnw"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </li>
+        </ul>
+      </div>
+    ),
+  },
   responsiveDesign: {
     title: 'Responsive design',
     content: (): ReactElement => (
