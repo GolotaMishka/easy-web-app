64156386016801cf74d7256f1795f34438f9ee23
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-05-24 10:51:06 +0300

    feat: calculator

--- src/data/config/development.ts
+++ src/data/config/development.ts
@@ -1,2 +1,2 @@
-// export const baseURL = 'http://localhost:4000/api';
-export const baseURL = 'https://easy-web-api.herokuapp.com/api';
+export const baseURL = 'http://localhost:4000/api';
+// export const baseURL = 'https://easy-web-api.herokuapp.com/api';
