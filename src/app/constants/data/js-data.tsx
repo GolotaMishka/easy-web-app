3120d493a0ec4140222fce4d7ea0aa65297c70d8
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-09-21 21:37:47 +0300

    fix: video link

--- src/app/constants/data/js-data.tsx
+++ src/app/constants/data/js-data.tsx
@@ -21,7 +21,7 @@ export const jsTopics = {
             <iframe
               style={{ width: '100%', marginTop: '15px' }}
               height="440"
-              src="https://www.youtube.com/embed/Df9sws99xIM"
+              src="https://www.youtube.com/embed/ti79mrSaMt0"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
