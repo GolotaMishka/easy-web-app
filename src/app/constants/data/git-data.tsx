8c64e8f836e423a48aa9435713424116cc13d5fd
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-10-09 11:16:52 +0300

    feat: gti flow

--- src/app/constants/data/git-data.tsx
+++ src/app/constants/data/git-data.tsx
@@ -3,15 +3,15 @@ import s from './styles.scss';
 
 export const gitTopics = {
   gitFlowWithReact: {
-    title: 'Basic git flow for your projects',
+    title: 'Basic git flow for react projects',
     content: (): ReactElement => (
       <div className={s.content}>
         <a
-          href="https://docs.google.com/document/d/146fZrPBqw_ORfp68dqQowVdCT6voou0ZfTxCMc0XeWg/edit"
+          href="https://docs.google.com/document/d/1GxGJt9yVENWTF3SnOucZaV7dYMp5YOFkVTrgL8pYeWA/edit?usp=sharing"
           target="_blank"
           rel="noreferrer"
         >
-          Simple guide
+          Git flow with React
         </a>
       </div>
     ),
