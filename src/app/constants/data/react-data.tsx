70fc4354fdb419fbff30bab58e92320e7d17fa54
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-10-14 22:41:50 +0300

    feat: new video

--- src/app/constants/data/react-data.tsx
+++ src/app/constants/data/react-data.tsx
@@ -13,6 +13,18 @@ export const reactTopics = {
             </a>
           </li>
           <li>
+            React State - это просто!
+            <iframe
+              style={{ width: '100%', marginTop: '15px' }}
+              height="440"
+              src="https://www.youtube.com/embed/qqeKBHnRR6A"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </li>
+          <li>
             Lesson
             <iframe
               style={{ width: '100%', marginTop: '15px' }}
