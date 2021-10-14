cb4950309ef101be6ae9705d6a887bc4e374d628
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-10-14 21:22:38 +0300

    feat: state

--- src/app/constants/data/react-data.tsx
+++ src/app/constants/data/react-data.tsx
@@ -2,6 +2,32 @@ import React, { ReactElement } from 'react';
 import s from './styles.scss';
 
 export const reactTopics = {
+  componentsState: {
+    title: 'State',
+    content: (): ReactElement => (
+      <div className={s.content}>
+        <ul>
+          <li>
+            <a href="https://www.w3schools.com/react/react_state.asp" target="_blank" rel="noreferrer">
+              State
+            </a>
+          </li>
+          <li>
+            Lesson
+            <iframe
+              style={{ width: '100%', marginTop: '15px' }}
+              height="440"
+              src="https://www.youtube.com/embed/6hAUJMIkMes"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </li>
+        </ul>
+      </div>
+    ),
+  },
   componentsProps: {
     title: 'Components, props',
     content: (): ReactElement => (
