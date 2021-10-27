6b0dd66c158ea739ae16f643de1170e1cd615305
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-10-27 21:14:37 +0300

    feat: routing

--- src/app/constants/data/react-data.tsx
+++ src/app/constants/data/react-data.tsx
@@ -2,6 +2,42 @@ import React, { ReactElement } from 'react';
 import s from './styles.scss';
 
 export const reactTopics = {
+  routing: {
+    title: 'Routing',
+    content: (): ReactElement => (
+      <div className={s.content}>
+        <ul>
+          <li>
+            <a
+              href="https://www.digitalocean.com/community/tutorials/how-to-handle-routing-in-react-apps-with-react-router"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Tutorial
+            </a>
+          </li>
+          <li>
+            <a href="https://reactrouter.com/web/guides/quick-start" target="_blank" rel="noreferrer">
+              Documentation
+            </a>
+          </li>
+
+          <li>
+            Lesson
+            <iframe
+              style={{ width: '100%', marginTop: '15px' }}
+              height="440"
+              src="https://www.youtube.com/embed/gIl7WrsAsC0"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </li>
+        </ul>
+      </div>
+    ),
+  },
   lifecycle: {
     title: 'Lifecycle',
     content: (): ReactElement => (
